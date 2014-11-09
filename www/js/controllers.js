77236cd534d28a32daa432ec6cefc610761208b1
Ian <ian.orellana@usach.cl>
2014-11-09 01:39:50 -0300

    Vista índice y salas versión inicial

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,14 +33,11 @@ angular.module('starter.controllers', [])
   };
 })
 
+
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Buscar sala', id: 1 },
+    { title: 'Créditos', id: 2 }
   ];
 })
 
