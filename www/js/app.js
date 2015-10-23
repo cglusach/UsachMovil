e0eb11389ce1340e57d11878790c8700fac841eb
Daniel Gacitua <danielgacituav@gmail.com>
2015-10-23 00:40:42 -0300

    Limpieza de Código Javascript

--- www/js/app.js
+++ www/js/app.js
@@ -1,128 +1,113 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+angular.module('umovil', ['ionic', 'ngCordova', 'umovil.controllers', 'umovil.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
 
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+		if(window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
 
-    //opciones = ProcesadorOpciones.getOpciones();
-    //console.log(opciones);
-    //db = window.sqlitePlugin.openDatabase({name: "umovil.sqlite", location: 2});
-  });
+		//opciones = ProcesadorOpciones.getOpciones();
+		//console.log(opciones);
+		//db = window.sqlitePlugin.openDatabase({name: "umovil.sqlite", location: 2});
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-  .state('umovil', {
-      url: "/umovil",
-      abstract: true,
-      templateUrl: "app/um_sidemenu.html"
-    })
+	.state('umovil', {
+		url: "/umovil",
+		abstract: true,
+		templateUrl: "app/um_sidemenu.html"
+	})
 
-  .state('umovil.inicio', {
-      url: "/inicio",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_inicio.html"
-        }
-      }
-    })
+	.state('umovil.inicio', {
+		url: "/inicio",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_inicio.html"
+			}
+		}
+	})
 
-  .state('umovil.opciones', {
-      url: "/opciones",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_opciones.html"
-        }
-      }
-    })
+	.state('umovil.opciones', {
+		url: "/opciones",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_opciones.html"
+			}
+		}
+	})
 
-  .state('umovil.ayuda', {
-      url: "/ayuda",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_ayuda.html"
-        }
-      }
-    })
+	.state('umovil.ayuda', {
+		url: "/ayuda",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_ayuda.html"
+			}
+		}
+	})
 
-  .state('umovil.buscarsala', {
-      url: "/buscarsala",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_buscarsala.html"
-        }
-      }
-    })
+	.state('umovil.buscarsala', {
+		url: "/buscarsala",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_buscarsala.html"
+			}
+		}
+	})
 
-  .state('umovil.lugares', {
-      url: "/lugares",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_lugares.html"
-        }
-      }
-    })
-  
-  /*
-  .state('umovil.ingenieria', {
-      url: "/ingenieria",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_ingenieria.html"
-        }
-      }
-    })
-  */
+	.state('umovil.lugares', {
+		url: "/lugares",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_lugares.html"
+			}
+		}
+	})
 
-  .state('umovil.links', {
-      url: "/links",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_links.html"
-        }
-      }
-    })
+	.state('umovil.links', {
+		url: "/links",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_links.html"
+			}
+		}
+	})
 
-  .state('umovil.acercade', {
-      url: "/acercade",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_acercade.html"
-        }
-      }
-    })
+	.state('umovil.acercade', {
+		url: "/acercade",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_acercade.html"
+			}
+		}
+	})
 
-  .state('salas', {
-      url: "/salas",
-      abstract: true,
-      templateUrl: "app/um_salasmenu.html"
-    })
+	.state('salas', {
+		url: "/salas",
+		abstract: true,
+		templateUrl: "app/um_salasmenu.html"
+	})
 
 
-  .state('salas.resultadosala', {
-      url: "/resultadosala/:nombre",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/um_resultadosala.html"
-        }
-      }
-    })
-	
+	.state('salas.resultadosala', {
+		url: "/resultadosala/:nombre",
+		views: {
+			'menuContent' :{
+				templateUrl: "app/um_resultadosala.html"
+			}
+		}
+	})
+
 	$urlRouterProvider.otherwise('/umovil/inicio');
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,144 +1,143 @@
-angular.module('starter.services', [])
+angular.module('umovil.services', [])
 
 .factory('GETservice', function($http, $q, FactoriaOpciones) {
 	//en teoria si resulta la recoleccion de datos, deberia de estar latitud y longitud en dato.latitud, dato.longitud
 	var lugar = {
-	  estado:'',
-	  valido:'',
-	  nombre:'',
-	  piso:'',
-	  tipo:'',
-	  latitud:'',
-	  longitud:'',
-	  metroOrigen:'',
-	  rutaCorta: [ { latitud:'', longitud:'' } ],
-	  rutaLarga: [ { latitud:'', longitud:'' } ]
+		estado:'',
+		valido:'',
+		nombre:'',
+		piso:'',
+		tipo:'',
+		latitud:'',
+		longitud:'',
+		metroOrigen:'',
+		rutaCorta: [ { latitud:'', longitud:'' } ],
+		rutaLarga: [ { latitud:'', longitud:'' } ]
 	};
 
 	return {
-			fetchLugar: function(input) {
-				console.log("UrlConsulta: " + FactoriaOpciones.getUrlConsulta());
-				console.log("SemestreConsulta: " + FactoriaOpciones.getSemestre());
+		fetchLugar: function(input) {
+			console.log("UrlConsulta: " + FactoriaOpciones.getUrlConsulta());
+			console.log("SemestreConsulta: " + FactoriaOpciones.getSemestre());
 
-				//por defecto
-				var urlBase = FactoriaOpciones.getUrlConsulta();
+			var urlBase = FactoriaOpciones.getUrlConsulta();
 
-				url1 = urlBase + "/lugar/buscar/" + input;
-				url2 = urlBase + "/coordenada/buscar/" + input;
-				url3 = urlBase + "/coordenadas/minimo/" + input;
-				url4 = urlBase + "/coordenadas/largo/" + input;
+			url1 = urlBase + "/lugar/buscar/" + input;
+			url2 = urlBase + "/coordenada/buscar/" + input;
+			url3 = urlBase + "/coordenadas/minimo/" + input;
+			url4 = urlBase + "/coordenadas/largo/" + input;
 
-				var req0 = $http.get(url1);
-				var req1 = $http.get(url2);
-				var req2 = $http.get(url3);
-				var req3 = $http.get(url4);
+			var req0 = $http.get(url1);
+			var req1 = $http.get(url2);
+			var req2 = $http.get(url3);
+			var req3 = $http.get(url4);
 
-				return $q.all([req0, req1, req2, req3]).then(function(obj) {
-			      if (obj[0].data.instance === 'Just') {
-			        lugar.nombre = obj[0].data.slot1.nombre;
-			        lugar.piso = obj[0].data.slot1.piso;
-			        lugar.tipo = obj[0].data.slot1.tipo;
-			        //console.log(lugar.nombre);
-			        //console.log(lugar.piso);
-			        //console.log(lugar.tipo);
-			      }
-			      else {
-			      	lugar.estado = "Lugar no existe";
-			      	lugar.valido = false;
-			      	return lugar;
-			      }
+			return $q.all([req0, req1, req2, req3]).then(function(obj) {
+				if (obj[0].data.instance === 'Just') {
+					lugar.nombre = obj[0].data.slot1.nombre;
+					lugar.piso = obj[0].data.slot1.piso;
+					lugar.tipo = obj[0].data.slot1.tipo;
+					//console.log(lugar.nombre);
+					//console.log(lugar.piso);
+					//console.log(lugar.tipo);
+				}
+				else {
+					lugar.estado = "Lugar no existe";
+					lugar.valido = false;
+					return lugar;
+				}
 
-			      if (obj[1].data.instance === 'Just') {
-			        lugar.latitud = obj[1].data.slot1.latitud;
-			        lugar.longitud = obj[1].data.slot1.longitud;
-			        //console.log(lugar.latitud);
-			        //console.log(lugar.longitud);
-			      }
-			      else {
-			      	lugar.estado = "Lugar no existe";
-			      	lugar.valido = false;
-			      	return lugar;
-			      }
+				if (obj[1].data.instance === 'Just') {
+					lugar.latitud = obj[1].data.slot1.latitud;
+					lugar.longitud = obj[1].data.slot1.longitud;
+					//console.log(lugar.latitud);
+					//console.log(lugar.longitud);
+				}
+				else {
+					lugar.estado = "Lugar no existe";
+					lugar.valido = false;
+					return lugar;
+				}
 
-			      if (obj[2].data.instance === 'Just') {
-			      	lugar.rutaCorta.length = 0;	// IMPORTANTE: Vaciar el arreglo antes de volver a usarlo
-			      	for(var i=0; i<obj[2].data.slot1.length; i++) {
-			      		if (i === 0) {
-			      			if (parseFloat(obj[2].data.slot1[i][0])===-33.4525108565 && parseFloat(obj[2].data.slot1[i][1])===-70.6860424147) {
-			      				lugar.metroOrigen = "Universidad de Santiago";
-			      			}
-			      			else if (parseFloat(obj[2].data.slot1[i][0])===-33.4506444426 && parseFloat(obj[2].data.slot1[i][1])===-70.6792510615) {
-			      				lugar.metroOrigen = "Estación Central"
-			      			}
-			      			else {
-			      				lugar.metroOrigen = "Metro no válido";
-			      			}
-			      		}
-				        lugar.rutaCorta[i] = { latitud: obj[2].data.slot1[i][0], longitud: obj[2].data.slot1[i][1] };
-				    }
-			      }
-			      else {
-			      	lugar.estado = "Lugar no existe";
-			      	lugar.valido = false;
-			      	return lugar;
-			      }
+				if (obj[2].data.instance === 'Just') {
+					lugar.rutaCorta.length = 0;	// IMPORTANTE: Vaciar el arreglo antes de volver a usarlo
+					for(var i=0; i<obj[2].data.slot1.length; i++) {
+						if (i === 0) {
+							if (parseFloat(obj[2].data.slot1[i][0])===-33.4525108565 && parseFloat(obj[2].data.slot1[i][1])===-70.6860424147) {
+								lugar.metroOrigen = "Universidad de Santiago";
+							}
+							else if (parseFloat(obj[2].data.slot1[i][0])===-33.4506444426 && parseFloat(obj[2].data.slot1[i][1])===-70.6792510615) {
+								lugar.metroOrigen = "Estación Central"
+							}
+							else {
+								lugar.metroOrigen = "Metro no válido";
+							}
+						}
+						lugar.rutaCorta[i] = { latitud: obj[2].data.slot1[i][0], longitud: obj[2].data.slot1[i][1] };
+					}
+				}
+				else {
+					lugar.estado = "Lugar no existe";
+					lugar.valido = false;
+					return lugar;
+				}
 
-			      if (obj[3].data.instance === 'Just') {
-			      	lugar.rutaLarga.length = 0;	// IMPORTANTE: Vaciar el arreglo antes de volver a usarlo
-			      	for(var i=0; i<obj[3].data.slot1.length; i++) {
-				        lugar.rutaLarga[i] = { latitud: obj[3].data.slot1[i][0], longitud: obj[3].data.slot1[i][1] };
-				    }
-			      }
-			      else {
-			      	lugar.estado = "Lugar no existe";
-			      	lugar.valido = false;
-			      	return lugar;
-			      }
+				if (obj[3].data.instance === 'Just') {
+					lugar.rutaLarga.length = 0;	// IMPORTANTE: Vaciar el arreglo antes de volver a usarlo
+					for(var i=0; i<obj[3].data.slot1.length; i++) {
+						lugar.rutaLarga[i] = { latitud: obj[3].data.slot1[i][0], longitud: obj[3].data.slot1[i][1] };
+					}
+				}
+				else {
+					lugar.estado = "Lugar no existe";
+					lugar.valido = false;
+					return lugar;
+				}
 
-			      lugar.estado = "¡Lugar encontrado!"
-			      lugar.valido = true;
-  				  return lugar;
-			  	}, function(err) {
-					console.error('ERR', err);
-					//alert("ERROR: No se pueden obtener los datos del lugar a buscar");
-					return null;
-			   	});
-			},
-			getLugar: function() {
+				lugar.estado = "¡Lugar encontrado!"
+				lugar.valido = true;
 				return lugar;
-			},
-		    getNombre: function() {
-				return lugar.nombre;
-			},
-			getPeriodo: function() {
-				var periodo = FactoriaOpciones.getSemestre();
-				return periodo;
-			}
+			}, function(err) {
+				console.error('ERR', err);
+				//alert("ERROR: No se pueden obtener los datos del lugar a buscar");
+			return null;
+			});
+		},
+		getLugar: function() {
+			return lugar;
+		},
+		getNombre: function() {
+			return lugar.nombre;
+		},
+		getPeriodo: function() {
+			var periodo = FactoriaOpciones.getSemestre();
+			return periodo;
+		}
 	}
 })
 
 .factory('$localstorage', ['$window', function($window) {
-  return {
-    set: function(key, value) {
-      $window.localStorage[key] = value;
-    },
-    get: function(key, defaultValue) {
-      return $window.localStorage[key] || defaultValue;
-    },
-    setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
-    },
-    getObject: function(key) {
-      return JSON.parse($window.localStorage[key] || '{}');
-    }
-  }
+	return {
+		set: function(key, value) {
+			$window.localStorage[key] = value;
+		},
+		get: function(key, defaultValue) {
+			return $window.localStorage[key] || defaultValue;
+		},
+		setObject: function(key, value) {
+			$window.localStorage[key] = JSON.stringify(value);
+		},
+		getObject: function(key) {
+			return JSON.parse($window.localStorage[key] || '{}');
+		}
+	}
 }])
 
 .service('ProcesadorOpciones', ['$localstorage', function($localstorage) {
 	return {
 		getDefault: function() {
 			return {
-			 	UrlConsulta: "https://salasusach.herokuapp.com/",
+				UrlConsulta: "https://salasusach.herokuapp.com/",
 				Semestre: "2015-02",
 				ModoOffline: false,
 				Geolocalizacion: false
@@ -148,14 +147,14 @@ angular.module('starter.services', [])
 			$localstorage.setObject('opcionesGuardadas', this.getDefault());
 		},		
 		getOpciones: function() {
-  			if (angular.equals({},$localstorage.getObject('opcionesGuardadas'))) {
-  				//console.log("Cargando opciones por defecto...");
-  				return this.getDefault();
-  			}
-  			else {
-  				//console.log("Cargando opciones guardadas...");
-  				return $localstorage.getObject('opcionesGuardadas');
-  			}
+			if (angular.equals({},$localstorage.getObject('opcionesGuardadas'))) {
+				//console.log("Cargando opciones por defecto...");
+				return this.getDefault();
+			}
+			else {
+				//console.log("Cargando opciones guardadas...");
+				return $localstorage.getObject('opcionesGuardadas');
+			}
 		},
 		setOpciones: function(valores) {
 			console.log("Guardando opciones...");
@@ -185,9 +184,9 @@ angular.module('starter.services', [])
 	return {
 		soportaAlmacenamientoHTML5: function() {
 			try {
-    			return 'localStorage' in window && window['localStorage'] !== null;
-  			}
-  			catch (e) {
+				return 'localStorage' in window && window['localStorage'] !== null;
+			}
+			catch (e) {
 				return false;
 			}			
 		}
